7cdf56d0aff68a5497bb489c4152a88d655ebf82
David BRASSELY <brasseld@gmail.com>
2012-09-14 22:30:12 +0200

    Example documentation

---
+++ assets/js/application.js
@@ -0,0 +1,24 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+!function ($) {
+
+  $(function(){
+
+    var $window = $(window)
+	
+    // side bar
+    $('.bs-docs-sidenav').affix({
+      offset: {
+        top: function () { return $window.width() <= 980 ? 290 : 210 }
+      , bottom: 270
+      }
+    })
+
+    // make code pretty
+    window.prettyPrint && prettyPrint()
+	
+  })
+
+}(window.jQuery)
